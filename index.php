d84f87f3bad807779ad591b58e8d6fc11c08d621
Houseisbck <Houseisback@yandex.ru>
2021-08-23 13:53:03 +0300

    fix

--- index.php
+++ index.php
@@ -4,26 +4,40 @@ require './vendor/autoload.php';
 use RetailCrm\Api\Interfaces\ClientExceptionInterface;
 use RetailCrm\Api\Interfaces\ApiExceptionInterface;
 use RetailCrm\Api\Factory\SimpleClientFactory;
-use RetailCrm\Api\Model\Entity\Customers\Customer;
-use RetailCrm\Api\Model\Request\Customers\CustomersCreateRequest;
 use RetailCrm\Api\Model\Entity\Orders\Order;
 use RetailCrm\Api\Model\Request\Orders\OrdersCreateRequest;
+use RetailCrm\Api\Model\Entity\Orders\Items\OrderProduct;
+use RetailCrm\Api\Model\Entity\Orders\Items\Offer;
+use RetailCrm\Api\Model\Entity\CustomersCorporate\Company;
 
 $client = SimpleClientFactory::createClient('https://superposuda.retailcrm.ru/', 'QlnRWTTWw9lv3kjxy1A8byjUmBQedYqb');
 $order = new Order();
+$offer = new Offer();
 $request = new OrdersCreateRequest();
+$item = new OrderProduct();
+$company = new Company();
 
-$order->lastName      = 'Kovach';
-$order->firstName     = 'Ivan';
-$order->patronymic = 'Victorovich';
-$order->orderType     = 'fizik';
-$order->orderMethod = 'test';
+$offer->article = 'AZ105R';
+
+$company->brand = 'Azalita';
+
+$item->offer = $offer;
+$item->productName = 'Маникюрный набор AZ105R Azalita';
+
 $order->number = '17072001';
+$order->status = 'trouble';
+$order->orderType = 'fizik';
+$order->orderMethod = 'test';
+$order->lastName = 'Kovach';
+$order->firstName = 'Ivan';
+$order->patronymic = 'Victorovich';
 $order->customFields = ['prim' => 'тестовое задание'];
-$order->customerComment = ''
+$order->customerComment = 'https://github.com/Houseisbck/TestKolosov';
+$order->items = [$item];
+$order->company = $company;
 
 $request->order = $order;
-$request->site  = 'test';
+$request->site = 'test';
 
 try {
     $response = $client->orders->create($request);
